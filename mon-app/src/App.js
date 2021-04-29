75ad36c7b0665ee183bb49734e0311432c08a8ad
Faiza <berrichif0717@gmail.com>
2021-04-29 13:05:45 +0200

    ajout function pour changer les states statiquement et un input pour changer les states dynamiquement

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -5,13 +5,43 @@ class App extends Component {//j'ai changé la fonction en class, par choix just
   state={
     titre:"Mon catalogues voitures"
   }
+
+  changeTitle =(e) => {
+    this.setState({
+      titre:"mon nouveau titre"
+    })//premiere méthode pour changer le state de façon statique
+  }
+
+  changeViaParam =(titre) => {//deuxième méthode pour changer le state de façon statique mais avec un paramétre , jai passé le non titre à l'intérrier des mes param,
+    this.setState({
+      titre:titre
+    })
+  }
+
+  changeViaBind =(param)=>{//troisème méthode pour changer le titre via la méthode bind
+    this.setState({
+      titre:param
+    })
+  }
+
+  changeViaInput = (e)=>{
+    this.setState({
+      titre:e.target.value
+    })
+  }
+
+
   render(){
   return (
     <div className="App">
     <Mycars title={this.state.titre}/>
+    <button onClick={this.changeTitle}>changer le nom en dur</button>
+    <button onClick={() => this.changeViaParam("titre changé via param")}>changer le nom via param</button>
+    <button onClick={this.changeViaBind.bind(this,'titre via bind')}>changer le nom via bind</button>
+    <input type='text' onChange={this.changeViaInput} value={this.state.titre}></input>
     </div>
   );//j'ai importé la class Mycars à l'intérrieur il avait un h1 Hello React
-};
-};
+};//pour faire fonctionner la méthode viaParam, il faut mettre une fonction autoinvoqué avant this
+};//pour faire fonctionner la méthode viaBind, j'ai utilisé bind(this,"text ici") pour faire fonctionner la fonction
 
 export default App;
--- mon-app/src/components/Mycars.js
+++ mon-app/src/components/Mycars.js
@@ -3,19 +3,27 @@ import React, {Component } from "react";
 import Car from './Cars';//j'ai importé le composant que j'ai crée dans Cars.js
 
 class Mycars extends React.Component {
+
+    noCopy=()=>{//je crée une fonction fléchée qui affiche une alert pour ne pas copier 
+      alert("merci de ne pas copier le texte");
+    }
+    
+
     
     render() {
  
         return(
             <div>
                 <h1>{this.props.title}</h1>
+                 
+                <p onCopy={this.noCopy}>Bonjour je suis votre conseiller</p>
                 <Car color="red">Ford</Car>
                 <Car color="green"></Car>
                 <Car color="">Opel</Car>
             </div>
         )//J'ai écrit le nom des voiture à l'intérieur des balises Car , mais il s'affiche pas car besoin de props de la fonction Car
     }//je peu x ajouter des propriété a car soit en children(exemple marque de voiture) soit en paramétre comme dans l'exemple de color
-}
+}//j'ai rajouté l'évenement onCopy sur mon p, pour empecher la copie , pour écrire du Js dans le jsx j'utilise les acollades{},je mets toujour this., 
 
 
 export default Mycars;//tjr j'export ma fonction pour pouvoir l'importer ailleur
